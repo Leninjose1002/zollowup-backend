678b52f5cfa598181ccfe62bc08a6e8b56e07280
Pragati Singh <pragatisingh0417@gmail.com>
2025-06-09 18:23:11 +0530

    updated code

--- config/passport.js
+++ config/passport.js
@@ -15,7 +15,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/users/google/callback",
+      callbackURL: "process.env.GOOGLE_CALLBACK_URL_USER",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {

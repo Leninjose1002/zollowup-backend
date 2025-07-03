bcb0e74bd5f95e4d72b9dc1808b5b79235ea08f0
Pragati Singh <pragatisingh0417@gmail.com>
2025-07-03 10:36:25 +0530

    updated url in server file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://zollowup-16nd.vercel.app/",
+      "https://zollowup-16nd.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,

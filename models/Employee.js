0e6fc782ab9f602000cedb47272cd298d037c8a4
Pragati Singh <pragatisingh0417@gmail.com>
2025-07-26 18:20:22 +0530

    updated backend

--- models/Employee.js
+++ models/Employee.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const employeeSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },  
   password: { type: String, required: false },             
-  createdAt: { type: Date, default: Date.now },  
-  resetPasswordToken: String,
-resetPasswordExpire: Date,
-         
+  createdAt: { type: Date, default: Date.now },           
 });
 
 module.exports = mongoose.model("Employee", employeeSchema);
